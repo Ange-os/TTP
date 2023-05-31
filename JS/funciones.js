34bc50c28e0a02d0909328f8e787cf8a0e8fb3cc
angel <mather@.com>
2023-05-31 20:19:18 -0300

    primer comit

---
+++ JS/funciones.js
@@ -0,0 +1,26 @@
+
+const socialLinks = document.querySelectorAll('.social-icons a');
+socialLinks.forEach(link => {
+    link.addEventListener('click', shareOnSocialMedia);
+});
+
+function shareOnSocialMedia(e) {
+    e.preventDefault();
+    const url = this.href; 
+    window.open(url, '_blank', 'width=600,height=400');
+}
+
+
+
+document.getElementById("btnEnviarWhatsApp").addEventListener("click", () => {
+    const mensaje = document.getElementById('mensaje')
+
+    let numero = "2664025070"; // Ingresa el número de teléfono aquí
+    let texto = mensaje.value
+
+            // Generar el enlace de WhatsApp
+    var url = "https://api.whatsapp.com/send?phone=" + numero + "&text=" + encodeURIComponent(texto);
+
+            // Abrir WhatsApp en una nueva pestaña
+    window.open(url);
+})
