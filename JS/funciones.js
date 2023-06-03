fc824535bb97375232c18e547de22bee7de4fff9
angel <mather@.com>
2023-06-03 13:40:40 -0300

    rebuscado pero no solucionado del todo

--- JS/funciones.js
+++ JS/funciones.js
@@ -11,28 +11,16 @@ function shareOnSocialMedia(e) {
 }
 
 
+const btnwts = document.getElementById("btnEnviarWhatsApp")
+    btnwts.addEventListener('click', function() {
+        const mensaje = document.getElementById('mensaje')
 
-document.getElementById("btnEnviarWhatsApp").addEventListener("click", () => {
-    const mensaje = document.getElementById('mensaje')
+        let numero = "2664025070"; // Ingresa el número de teléfono aquí
+        let texto = mensaje.value
 
-    let numero = "2664025070"; // Ingresa el número de teléfono aquí
-    let texto = mensaje.value
+                // Generar el enlace de WhatsApp
+        var url = "https://api.whatsapp.com/send?phone=" + numero + "&text=" + encodeURIComponent(texto);
 
-            // Generar el enlace de WhatsApp
-    var url = "https://api.whatsapp.com/send?phone=" + numero + "&text=" + encodeURIComponent(texto);
-
-            // Abrir WhatsApp en una nueva pestaña
-    window.open(url);
-})
-
-document.addEventListener('DOMContentLoaded', function() {
-    var videoPortada = document.querySelector('.galeria-videos .video-portada');
-    
-        videoPortada.addEventListener('touchstart', function() {
-        videoPortada.classList.add('hover');
-        });
-    
-        videoPortada.addEventListener('touchend', function() {
-        videoPortada.classList.remove('hover');
-        });
-    });
+                // Abrir WhatsApp en una nueva pestaña
+        window.open(url);
+    })
